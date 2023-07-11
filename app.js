6fa4c75a80840ef398de0232b44b74e749b2fcec
MateoNerli <mnerli2003@gmail.com>
2023-07-11 20:46:17 -0300

    feat: se agrego .gitignore,app.js,views

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const puerto = 3000;
+
+app.listen(puerto, () => {
+  console.log("Aplicación corriendo en el puerto 3000");
+});
+
+app.use(express.json());
+app.use("/", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
