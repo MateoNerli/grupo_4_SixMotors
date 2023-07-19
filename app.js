73e88b43be1dacb4aec573bc9e84a6cd6d074b15
fedetz1 <fede031137@gmail.com>
2023-07-18 21:54:28 -0300

    nueva pagina de detalles

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
-app.get("/detalles", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/detalles.html"));
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/productDetail.html"));
+});
+
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/carrito.html"));
 });
