16208cec6b4ee3e0d8e2a2997fae08c9597487a0
MateoNerli <mnerli2003@gmail.com>
2023-07-26 16:46:25 -0300

    correcion de linkeos del home

--- app.js
+++ app.js
@@ -39,3 +39,7 @@ app.get("/carrito", (req, res) => {
 app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
+
+app.get("/autopartes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/autopartes.html"));
+});
