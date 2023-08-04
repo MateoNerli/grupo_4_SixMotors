4bdae3ef7d4a4d4bb06728f9988a721a3aa12a37
MateoNerli <mnerli2003@gmail.com>
2023-08-04 12:23:33 -0300

    menu de vista de todos los productos

--- public/javascript/autos.js
+++ public/javascript/autos.js
@@ -11,23 +11,8 @@ botonesCategorias.forEach((boton) => {
   boton.addEventListener("click", (e) => {
     botonesCategorias.forEach((boton) => boton.classList.remove("active"));
     e.currentTarget.classList.add("active");
-
-    if (e.currentTarget.id != "todos") {
-      const productoCategoria = productos.find(
-        (producto) => producto.categoria.id === e.currentTarget.id
-      );
-      tituloPrincipal.innerText = productoCategoria.categoria.nombre;
-      const productosBoton = productos.filter(
-        (producto) => producto.categoria.id === e.currentTarget.id
-      );
-      cargarProductos(productosBoton);
-    } else {
-      tituloPrincipal.innerText = "Todos los productos";
-      cargarProductos(productos);
-    }
   });
 });
-
 //MENU
 const openMenu = document.querySelector("#open-menu");
 const closeMenu = document.querySelector("#close-menu");
