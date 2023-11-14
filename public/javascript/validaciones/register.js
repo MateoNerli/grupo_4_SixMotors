61dda2b357df3d44da2ef3dc396d8251e26e130a
MateoNerli <mnerli2003@gmail.com>
2023-11-14 09:11:40 -0300

    Se agregaron iconos en la validacion del register

--- public/javascript/validaciones/register.js
+++ public/javascript/validaciones/register.js
@@ -1,11 +1,21 @@
 const form = document.getElementById("formulario");
 
+//parentNode es el padre del elemento
 const validarNombre = () => {
   if (form.name.value == "" || form.name.value.length < 2) {
     form.name.parentNode.classList.add("is-invalid");
+    if (!form.name.parentNode.querySelector(".fa-exclamation-triangle")) {
+      var icon = document.createElement("i");
+      icon.classList.add("fa", "fa-exclamation-triangle");
+      form.name.parentNode.appendChild(icon);
+    }
     return false;
   } else {
     form.name.parentNode.classList.remove("is-invalid");
+    var icon = form.name.parentNode.querySelector(".fa-exclamation-triangle");
+    if (icon) {
+      form.name.parentNode.removeChild(icon);
+    }
     return true;
   }
 };
@@ -13,9 +23,20 @@ const validarNombre = () => {
 const validarApellido = () => {
   if (form.lastname.value == "" || form.lastname.value.length < 2) {
     form.lastname.parentNode.classList.add("is-invalid");
+    if (!form.lastname.parentNode.querySelector(".fa-exclamation-triangle")) {
+      var icon = document.createElement("i");
+      icon.classList.add("fa", "fa-exclamation-triangle");
+      form.lastname.parentNode.appendChild(icon);
+    }
     return false;
   } else {
     form.lastname.parentNode.classList.remove("is-invalid");
+    var icon = form.lastname.parentNode.querySelector(
+      ".fa-exclamation-triangle"
+    );
+    if (icon) {
+      form.lastname.parentNode.removeChild(icon);
+    }
     return true;
   }
 };
@@ -23,9 +44,18 @@ const validarApellido = () => {
 const validarUser = () => {
   if (form.user.value == "" || form.user.value.length < 2) {
     form.user.parentNode.classList.add("is-invalid");
+    if (!form.user.parentNode.querySelector(".fa-exclamation-triangle")) {
+      var icon = document.createElement("i");
+      icon.classList.add("fa", "fa-exclamation-triangle");
+      form.user.parentNode.appendChild(icon);
+    }
     return false;
   } else {
     form.user.parentNode.classList.remove("is-invalid");
+    var icon = form.user.parentNode.querySelector(".fa-exclamation-triangle");
+    if (icon) {
+      form.user.parentNode.removeChild(icon);
+    }
     return true;
   }
 };
@@ -33,9 +63,18 @@ const validarUser = () => {
 const validarEmail = () => {
   if (!validarQueSeaEmail(form.email.value) || form.email.value == "") {
     form.email.parentNode.classList.add("is-invalid");
+    if (!form.email.parentNode.querySelector(".fa-exclamation-triangle")) {
+      var icon = document.createElement("i");
+      icon.classList.add("fa", "fa-exclamation-triangle");
+      form.email.parentNode.appendChild(icon);
+    }
     return false;
   } else {
     form.email.parentNode.classList.remove("is-invalid");
+    var icon = form.email.parentNode.querySelector(".fa-exclamation-triangle");
+    if (icon) {
+      form.email.parentNode.removeChild(icon);
+    }
     return true;
   }
 };
@@ -43,9 +82,20 @@ const validarEmail = () => {
 const validarPais = () => {
   if (form.country.value == "") {
     form.country.parentNode.classList.add("is-invalid");
+    if (!form.country.parentNode.querySelector(".fa-exclamation-triangle")) {
+      var icon = document.createElement("i");
+      icon.classList.add("fa", "fa-exclamation-triangle");
+      form.country.parentNode.appendChild(icon);
+    }
     return false;
   } else {
     form.country.parentNode.classList.remove("is-invalid");
+    var icon = form.country.parentNode.querySelector(
+      ".fa-exclamation-triangle"
+    );
+    if (icon) {
+      form.country.parentNode.removeChild(icon);
+    }
     return true;
   }
 };
@@ -53,9 +103,18 @@ const validarPais = () => {
 const validarCel = () => {
   if (form.cel.value == "" || !sonTodosNumeros(form.cel.value)) {
     form.cel.parentNode.classList.add("is-invalid");
+    if (!form.cel.parentNode.querySelector(".fa-exclamation-triangle")) {
+      var icon = document.createElement("i");
+      icon.classList.add("fa", "fa-exclamation-triangle");
+      form.cel.parentNode.appendChild(icon);
+    }
     return false;
   } else {
     form.cel.parentNode.classList.remove("is-invalid");
+    var icon = form.cel.parentNode.querySelector(".fa-exclamation-triangle");
+    if (icon) {
+      form.cel.parentNode.removeChild(icon);
+    }
     return true;
   }
 };
@@ -67,10 +126,24 @@ const validarContrasena = () => {
     !validarValorContrasena(form.password.value)
   ) {
     form.password.parentNode.classList.add("is-invalid");
-    return false;
-  } else {
-    form.password.parentNode.classList.remove("is-invalid");
-    return true;
+    if (!form.password.parentNode.querySelector(".fa-exclamation-triangle")) {
+      form.password.parentNode.classList.add("is-invalid");
+      if (!form.password.parentNode.querySelector(".fa-exclamation-triangle")) {
+        var icon = document.createElement("i");
+        icon.classList.add("fa", "fa-exclamation-triangle");
+        form.password.parentNode.appendChild(icon);
+      }
+      return false;
+    } else {
+      form.password.parentNode.classList.remove("is-invalid");
+      var icon = form.password.parentNode.querySelector(
+        ".fa-exclamation-triangle"
+      );
+      if (icon) {
+        form.password.parentNode.removeChild(icon);
+      }
+      return true;
+    }
   }
 };
 
@@ -80,9 +153,51 @@ const validarRepetirContrasena = () => {
     form.repeatPassword.value != form.password.value
   ) {
     form.repeatPassword.parentNode.classList.add("is-invalid");
+    if (
+      !form.repeatPassword.parentNode.querySelector(".fa-exclamation-triangle")
+    ) {
+      var icon = document.createElement("i");
+      icon.classList.add("fa", "fa-exclamation-triangle");
+      form.repeatPassword.parentNode.appendChild(icon);
+    }
     return false;
   } else {
     form.repeatPassword.parentNode.classList.remove("is-invalid");
+    var icon = form.repeatPassword.parentNode.querySelector(
+      ".fa-exclamation-triangle"
+    );
+    if (icon) {
+      form.repeatPassword.parentNode.removeChild(icon);
+    }
+    return true;
+  }
+};
+
+const validarPassword = () => {
+  const password = form.password.value;
+  const repeatPassword = form.repeatPassword.value;
+  if (password !== repeatPassword) {
+    form.password.parentNode.classList.add("is-invalid");
+    form.repeatPassword.parentNode.classList.add("is-invalid");
+    if (!form.password.parentNode.querySelector(".fa-exclamation-triangle")) {
+      var icon = document.createElement("i");
+      icon.classList.add("fa", "fa-exclamation-triangle");
+      form.password.parentNode.appendChild(icon);
+      form.repeatPassword.parentNode.appendChild(icon.cloneNode());
+    }
+    return false;
+  } else {
+    form.password.parentNode.classList.remove("is-invalid");
+    form.repeatPassword.parentNode.classList.remove("is-invalid");
+    var icon = form.password.parentNode.querySelector(
+      ".fa-exclamation-triangle"
+    );
+    if (icon) {
+      form.password.parentNode.removeChild(icon);
+      form.repeatPassword.parentNode.removeChild(
+        form.repeatPassword.parentNode.querySelector(".fa-exclamation-triangle")
+      );
+    }
     return true;
   }
 };
