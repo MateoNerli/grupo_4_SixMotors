8edf5f7ac4b24dbcedbc0f194704c1e9749dd103
MateoNerli <mnerli2003@gmail.com>
2023-11-29 00:15:25 -0300

    Config render

--- app.js
+++ app.js
@@ -20,9 +20,6 @@ app.use(
     secret: "Shhh, It's a secret",
     resave: false,
     saveUninitialized: true,
-    store: new (require("connect-pg-simple")(session))({
-      conString: process.env.DB_HOST,
-    }),
   })
 );
 
